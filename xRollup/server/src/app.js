a58d3743e11ffc59e54b47ee1a7e4fe198b10b8a
Thomas Spofford <thomas@Thomass-MacBook-Pro.local>
2019-03-09 14:37:22 +0100

    switch to jayson

--- xRollup/server/src/app.js
+++ xRollup/server/src/app.js
@@ -1,24 +1,23 @@
 // External Packages
-const express = require('express')
-const app = express()
 const winston = require('winston')
 const Web3 = require('web3');
+var jayson = require('jayson');
 
-//Internal Services
+// Internal Services
 const EventWatcher = require('./eventWatcher');
 const StateManager = require('./stateManager');
 const LogService = require('./logService');
 const ContractService = require('./contractService');
 
-const port = 3000;
+// ENV Variables
+const PORT = 3000;
 
-const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws'));
+// Logging Setup
+const files = new winston.transports.File({ filename: 'logfile.log' });
+const myconsole = new winston.transports.Console();
 
-    const files = new winston.transports.File({ filename: 'logfile.log' });
-  const myconsole = new winston.transports.Console();
-
-  winston.add(myconsole);
-  winston.add(files);
+winston.add(myconsole);
+winston.add(files);
 
 // subscription.unsubscribe((error, success) => {
 // if (error) return console.error(error);
@@ -27,6 +26,7 @@ const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura
 // });
 
 // Initialize Services
+const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws'));
 const logService = new LogService(winston);
 const stateManager = new StateManager(logService);
 const contractService = new ContractService(web3, stateManager);
@@ -35,14 +35,7 @@ const eventWatcher = new EventWatcher(web3, logService, contractService);
 eventWatcher.subscribeToBlocks(null);
 eventWatcher.subscribeToDeposit(null);
 
-const bodyParser = require('body-parser');
-app.use(bodyParser);
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/api/transfer', (req, res) => {
-    console.log("Transfer Request")
-    console.log(req.body);
-
+/* 
     const pubKey = req.body.pubKey;
     const signature = req.body.signature;
 
@@ -50,18 +43,24 @@ app.post('/api/transfer', (req, res) => {
     const nonce = req.body.nonce;
     const recipient = req.body.recipient;
     const amount = req.body.amount;
-})
-app.post('/api/withdraw', (req, res) => {
-    console.log("Withdraw Request")
-    console.log(req.body);
-    
-    const pubKey = req.body.pubKey;
-    const signature = req.body.signature;
+*/
 
-    // Transaction Data
-    const nonce = req.body.nonce;
-    const amount = req.body.amount;
-})
-app.get('/api/state', (req, res) => res.send('Current State!'))
+// Create Server
+var server = jayson.server({
+    add: function (args, callback) {
+        callback(null, args[0] + args[1]);
+    }
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+server.http().listen(PORT);
+
+// Create Client
+var client = jayson.client.http({
+    port: PORT
+  });
+  
+  // invoke "add"
+  client.request('add', [1, 1], function(err, response) {
+    if(err) throw err;
+    console.log(response.result); // 2
+  });
