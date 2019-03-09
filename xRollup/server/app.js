478e87c4ec1c9762db3a8ba9388feb0673f34a1e
Thomas Spofford <thomas@Thomass-MacBook-Pro.local>
2019-03-09 03:34:28 +0100

    server scaffolding

--- xRollup/server/app.js
+++ xRollup/server/app.js
@@ -2,9 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser');
+app.use(bodyParser);
+
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/api/transfer', (req, res) => res.send('Transfer!'))
-app.get('/api/withdraw', (req, res) => res.send('Withdraw!'))
+app.post('/api/transfer', (req, res) => {
+    console.log("Transfer Request")
+    console.log(req.body);
+
+    const nonce = req.body.nonce;
+    const signature = req.body.signature;
+    const recipient = req.body.recipient;
+    const amount = req.body.amount;
+})
+app.post('/api/withdraw', (req, res) => {
+    console.log("Withdraw Request")
+    console.log(req.body);
+
+    const nonce = req.body.nonce;
+    const signature = req.body.signature;
+    const amount = req.body.amount;
+})
 app.get('/api/state', (req, res) => res.send('Current State!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
