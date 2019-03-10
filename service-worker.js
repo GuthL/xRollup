ac8a1f7b53206740a60f67f542e2889ea1b9f23d
Anirudha Bose <ani07nov@gmail.com>
2019-03-10 03:59:45 +0100

    Update burner wallet distribution

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8d5d356a06cd0186d68d1d20376d318e.js"
+  "./precache-manifest.e402232df4d559bc4e5d40237d002466.js"
 );
 
 workbox.clientsClaim();
